d440579b850164002606055102b8681cc405395b
Adrian-P0gar <pogar.adi@gmail.com>
2019-10-30 11:35:03 +0200

    bla2

---
+++ roman.py
@@ -0,0 +1,17 @@
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+arabic = input("Number:")
+
+
+def roman_numbers():
+    for i in arabic:
+        if i == 1:
+            return "I"
+
+
+print(roman_numbers())
